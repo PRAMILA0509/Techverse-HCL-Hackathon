6a0f62df3bd013dbf3f0f7c279ee098fa0f254e6
Madhumitha Perumal <sugunavathisugunavathi423@gmail.com>
2026-04-01 11:36:16 +0530

    Added retail_website folder

---
+++ retail_website/src/main/java/com/backend/retail_website/RetailWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.backend.retail_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailWebsiteApplication.class, args);
+	}
+
+}
